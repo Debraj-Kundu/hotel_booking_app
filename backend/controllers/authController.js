7a178172c9d7ebab3b151bf258e1a5907d324719
Debraj Kundu <debrajk27@gmail.com>
2023-11-22 20:30:58 +0530

    user register & login api, correction in errors.js file

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,12 +1,17 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { createError } from "../utils/errors.js";
 
 const register = async (req, res, next) => {
-  const { username, email, password } = req.body;
   try {
+    const { username, email, password } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
     await User.create({
       username,
       email,
-      password,
+      password: hash,
     });
     res.status(201).json("User created successfully");
   } catch (error) {
@@ -14,4 +19,21 @@ const register = async (req, res, next) => {
   }
 };
 
-export { register };
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return next(createError(404, "User not found"));
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch)
+      return next(createError(400, "Username or password dosen't match"));
+
+    const {password:pass, isAdmin, ...otherDetails} = user._doc
+    res.status(201).json(otherDetails);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { register, login };
